c24aa54dae8792b62d67f9b99b3117ebf505a15c
lucasriccioppo <lucas.riccioppo@neppoadm.com.br>
2019-05-21 21:30:29 -0300

    put all files togheter. Finish project.

--- sudoku_generator.c
+++ sudoku_generator.c
@@ -3,36 +3,194 @@
 #include <time.h>
 #include <string.h>
 
-void generateSudoku(int n, int m, int mat[n][m]) {
-	int i, j, k, r, lineFlag, count, aux, linha, coluna;
+struct Sudoku {
+  int grid[9][9];
+  int solnGrid[9][9];
+  int guessNum[9];
+  int gridPos[81];
+};
+typedef struct Sudoku Sudoku;
 
-	for (i=0; i<n; i++) {
-   		for (j=0; j<m; j++) {
-	   		r = rand() % 9 + 1;
-			mat[i][j] = r;
-   		}
-   	}
 
-// todo
-   	for (i=0; i<n; i++) {
-   		for (j=0; j<m; j++) {
-   			lineFlag = 0;
-   			while (lineFlag==0){
-   				aux = 0;
-	   			r = rand() % 9 + 1;
-	   			for (k=0; k<9; k++) {
-	   				if (r == mat[i][k]) {
-	   					aux = aux + 1;
-	   				}
-	   			}
-	   			if (aux < 1) {
-	   				mat[i][j] = r;
-	   				lineFlag = 1;
-	   			}
-	   			
-	   		}
-   		}
-   	}
+int FindUnassignedLocation(int grid[9][9], int *row, int *col) {
+
+  int i = 0, j = 0;
+    for (i = 0; i < 9; i++) {
+        for (j = 0; j < 9; j++) {
+            if (grid[i][j] == 0) {
+                *row = i;
+                *col = j;
+                return 1;
+            }
+        }
+    }
+    *row = i;
+    *col = j;
+    return 0;
+}
+
+
+
+int UsedInRow(int grid[9][9], int row, int num) {
+
+    int col = 0;
+
+    for (col = 0; col < 9; col++) {
+        if (grid[row][col] == num)
+            return 1;
+    }
+
+    return 0;
+}
+
+int UsedInCol(int grid[9][9], int col, int num) {
+
+    int row = 0;
+
+    for (row = 0; row < 9; row++) {
+        if (grid[row][col] == num)
+            return 1;
+    }
+
+    return 0;
+}
+
+int UsedInBox(int grid[9][9], int boxStartRow, int boxStartCol, int num) {
+
+    int row, col;
+
+    for (row = 0; row < 3; row++) {
+        for (col = 0; col < 3; col++) {
+            if (grid[row+boxStartRow][col+boxStartCol] == num) {
+                return 1;
+            }
+        }
+    }
+
+    return 0;
+}
+
+
+int isSafe(int grid[9][9], int row, int col, int num) {
+
+    if (UsedInRow(grid, row, num)==0 && UsedInCol(grid, col, num)==0 && UsedInBox(grid, row - row%3 , col - col%3, num)==0) {
+      return 1;
+    } else {
+      return 0;
+    }
+}
+
+
+int solveGrid(Sudoku *puzzle) {
+
+    int row = 0, col = 0, num = 0;
+    if (FindUnassignedLocation(puzzle->grid, &row, &col) == 0) {
+       return 1;
+    }
+    
+    for (num = 0; num < 9; num++) {
+        
+        if (isSafe(puzzle->grid, row, col, puzzle->guessNum[num]) == 1) {
+            
+            puzzle->grid[row][col] = puzzle->guessNum[num];
+
+            if (solveGrid(puzzle)) {
+                return 1;
+            } else {
+              puzzle->grid[row][col] = 0;
+            }
+
+        }
+    }
+
+    return 0;
+
+}
+
+void createSeed(Sudoku *puzzle) {
+
+  int i, j;
+
+  solveGrid(puzzle);
+  
+  for(i=0;i<9;i++) {
+    for(j=0;j<9;j++) {
+      puzzle->solnGrid[i][j] = puzzle->grid[i][j];
+    }
+  }
+}
+
+void countSoln(Sudoku *puzzle, int *number) {
+  int row = 0, col = 0, i = 0;
+
+  if(FindUnassignedLocation(puzzle->grid, &row, &col) == 0) {
+    *number++;
+    return ;
+  }
+
+  for(i=0;i<9 && *number<2;i++) {
+      if( isSafe(puzzle->grid, row, col, puzzle->guessNum[i]) ) {
+        puzzle->grid[row][col] = puzzle->guessNum[i];
+        countSoln(puzzle, number);
+      }
+
+      puzzle->grid[row][col] = 0;
+  }
+
+}
+
+void genPuzzle(Sudoku *puzzle) {
+  int i, x, y, temp, check;
+
+  for(i=0;i<81;i++) {
+    x = (puzzle->gridPos[i])/9;
+    y = (puzzle->gridPos[i])%9;
+    temp = puzzle->grid[x][y];
+    puzzle->grid[x][y] = 0;
+
+    check=0;
+    countSoln(puzzle, &check);
+    if(check!=1) {
+      puzzle->grid[x][y] = temp;
+    }
+  }
+}
+
+void swap(int *list, int i, int j) {
+  int temp = list[i];
+  list[i] = list[j];
+  list[j] = temp;
+}
+
+void shuffle(int *list, int n) {
+  int i = 0, j = 0;
+  for ( i = n - 1; i >= 1; i--) {
+    j = rand() % (i + 1);
+
+    swap(list, i, j);
+  }
+}
+
+void initializePuzzle(Sudoku *puzzle) {
+  int i, j;
+
+  for(i=0;i<81;i++) {
+    puzzle->gridPos[i] = i;
+  }
+
+  shuffle(puzzle->gridPos, 81);
+
+  for(i=0;i<9;i++) {
+    puzzle->guessNum[i]=i+1;
+  }
+
+  shuffle(puzzle->guessNum, 9);
+
+  for (i=0; i<9; i++) {
+    for (j=0; j<9; j++) {
+      puzzle->grid[i][j] = 0;
+    }
+  }
 
 }
 
@@ -99,6 +257,14 @@ void persistGame(int game[9][9], char *fileName) {
    	fclose (fp);
 }
 
+void generateBaseGame(Sudoku *puzzle) {
+
+	initializePuzzle(puzzle);
+  	createSeed(puzzle);
+  	genPuzzle(puzzle);
+  	printGame(puzzle->solnGrid);
+}
+
 void generate(int originGame[9][9], int off, char *fileName) {
 	int i, j, line, col, game[9][9];
 
@@ -138,25 +304,22 @@ void generateHardGame(int originGame[9][9], char *fileName) {
 
 int main()
 {
-   	int list[9], i, j, r, exemplo[9];
-
-   	int mat[9][9] = { 	{5,3,4,6,7,8,9,1,2},{6,7,2,1,9,5,3,4,8},{1,9,8,3,4,2,5,6,7},
-						{8,5,9,7,6,1,4,2,3},{4,2,6,8,5,3,7,9,1},{7,1,3,9,2,4,8,5,6},
-						{9,6,1,5,3,7,2,8,4},{2,8,7,4,1,9,6,3,5},{3,4,5,2,8,6,1,7,9}};
+	Sudoku puzzle;
 
    	srand(time(NULL));
 
-   	generateEasyGame(mat, "easyGame_1.txt");
-   	generateEasyGame(mat, "easyGame_2.txt");
-   	generateEasyGame(mat, "easyGame_3.txt");
-   	generateMediumGame(mat, "mediumGame_1.txt");
-   	generateMediumGame(mat, "mediumGame_2.txt");
-   	generateMediumGame(mat, "mediumGame_3.txt");
-   	generateHardGame(mat, "hardGame_1.txt");
-   	generateHardGame(mat, "hardGame_2.txt");
-   	generateHardGame(mat, "hardGame_3.txt");
-   	
-   	// generateRandomGame(9,9, mat);
+   	generateBaseGame(&puzzle);
+
+   	generateEasyGame(puzzle.solnGrid, "easyGame_1.txt");
+   	generateEasyGame(puzzle.solnGrid, "easyGame_2.txt");
+   	generateEasyGame(puzzle.solnGrid, "easyGame_3.txt");
+   	generateMediumGame(puzzle.solnGrid, "mediumGame_1.txt");
+   	generateMediumGame(puzzle.solnGrid, "mediumGame_2.txt");
+   	generateMediumGame(puzzle.solnGrid, "mediumGame_3.txt");
+   	generateHardGame(puzzle.solnGrid, "hardGame_1.txt");
+   	generateHardGame(puzzle.solnGrid, "hardGame_2.txt");
+   	generateHardGame(puzzle.solnGrid, "hardGame_3.txt");
+
 
    	return 0;
 }
